51f7215a6561b60ae8bfeda790b48117d020a01e
Sai Ganesh <117970120+AKAspidey01@users.noreply.github.com>
2025-09-09 13:45:27 +0530

    intial Push

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home/Home'
+import Footer from './shared/Footer/Footer'
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import Header from './shared/Header/Header';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+  const [headerMarquee, setHeaderMarquee] = useState(false)
+
+   window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 200) {
+      setHeaderMarquee(true);
+    }
+    if (window.pageYOffset <= 100) {
+      setHeaderMarquee(false);
+    }
+  });
+
+  return (
+    <Router>
+      <Header headerMarquee={headerMarquee}/>
+      <Routes>
+        <Route path='/' Component={Home}/>
+      </Routes>
+      <Footer/>
+    </Router>
+  )
+}
+
+export default App
